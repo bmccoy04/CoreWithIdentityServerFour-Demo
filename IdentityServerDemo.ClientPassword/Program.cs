2a5b96674a71c0869dbd2ba0d8d63dedc7da49df
bmccoy04 <bryanmccoy04@gmail.com>
2018-02-18 19:31:59 -0600

    Added the projects that will be demo'd

---
+++ IdentityServerDemo.ClientPassword/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServerDemo.ClientPassword
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IdentityServerDemo.ClientSecret/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IdentityServerDemo.ClientSecret
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
